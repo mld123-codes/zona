198189b9b327be7859fe02ccd7859ae1d2bc0c61
mld123-codes <mlamined123@gmail.com>
2024-10-09 23:21:41 +0100

    Reviews

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useReducer, useState } from 'react';
 // import data from '../data';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
 const reducer = (state, action) => {
   switch (action.type) {
     case 'FETCH_REQUEST':
@@ -43,24 +45,15 @@ const HomeScreen = () => {
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product) => {
-            return (
-              <div key={product.slug} className='product'>
-                <Link to={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </Link>
-                <div className='product-info'>
-                  <Link to={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </Link>
-                  <p>
-                    <strong>${product.price}</strong>
-                  </p>
-                  <button>Add to cart</button>
-                </div>
-              </div>
-            );
-          })
+          <Row>
+            {products.map((product, i) => {
+              return (
+                <Col key={product.slug} sm={6} md={4} lg={3} className='mb-3'>
+                  <Product product={product}></Product>
+                </Col>
+              );
+            })}
+          </Row>
         )}
       </div>
     </div>
